55b58dae11e6d1397432bc58b89153324ca1a762
Leyla Mamedli <leyla.mamedli.97@mail.ru>
2020-01-12 16:58:18 +0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('.works-slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow: $(".arrow-prev"),
+    nextArrow: $(".arrow-next")
+});
